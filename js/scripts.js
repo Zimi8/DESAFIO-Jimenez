61f62b188e05946e34b1e628484ffcf0f16ad541
Ezequiel Jimenez <ezequielemilianojimenez@gmail.com>
2022-07-12 18:17:44 -0400

    Subiendo a Git

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+//MI INTENTO 
+
+let resultado = 0;
+
+function sumar(primerNumber, segundoNumber) {
+    resultado = primerNumber + segundoNumber
+}
+function promediar(resultado){
+    resultado= resultado / 2
+    
+}
+
+function mostrarConsola(mensaje){
+    console.log(mensaje)
+}
+//llamo la suma y luego mostrar
+
+
+promediar(7,9);
+mostrarConsola(resultado)
+
+
+
+
+
+
+
+
+
+
+
+//NOTAS DE LA CLASE
+
+/* let precio = 13000
+const producto = "Zapatotilla";
+function agregarAlCarrito(){
+    console.log ("Agregas el producto al carrito: "+ producto);
+}
+
+agregarAlCarrito()
+agregarAlCarrito()
+agregarAlCarrito()
+agregarAlCarrito()
+agregarAlCarrito()
+*/
+/*
+let nota1 = 0
+let nota2 = 0
+
+
+    function sumaDeNotas( nota1 + nota2) {
+        console.log("Resultado es " + sumaDeNotas);
+    }
+    sumaDeNotas()
+*******/
+/*  NO ME FUNCIONÓ
+let nota1=0;
+let nota2=0;
+
+
+const sumaResultado = nota1 + nota2;
+function suma(){
+    console.log (sumaResultado);
+}
+suma(1, 2);  */
+
+
+//ejemplo de coder
+ /*
+let resultado = 0;
+
+function sumar(primerNumber, segundoNumber) {
+    resultado = primerNumber + segundoNumber
+}
+
+function mostrarConsola(mensaje){
+    console.log(mensaje)
+}
+//llamo la suma y luego mostrar
+
+sumar(8,9);
+mostrarConsola(resultado);
+//fin ejemplo *******
+*/
+
